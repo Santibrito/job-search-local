bdc48166bca7172ad2c973286c12fe8636ba60ce
Santibrito <santiagobrito0615@gmail.com>
2024-03-11 20:56:53 -0300

    Project Initialization

---
+++ src/main/java/com/jobsearch/localjobsearch/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.jobsearch.localjobsearch.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    /**
+     * Configura y personaliza la generación de la documentación Swagger.
+     *
+     * @return Una instancia de Docket configurada para Swagger.
+     */
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+}
---
+++ src/test/java/com/jobsearch/localjobsearch/LocalJobSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobsearch.localjobsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalJobSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
